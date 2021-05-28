df352fe9a53fec27360cb5e4e8c11985268ccb0c
Auto-Club <84959631+Auto-Club@users.noreply.github.com>
2021-05-28 00:28:33 -0700

    Add files via upload

---
+++ AutoClubAboutPage/main.js
@@ -0,0 +1,4 @@
+
+function donatePage(){
+    window.location.replace("https://redondounionasb.myschoolcentral.com/asbworks/(S(jxklwosuljysl3zb4skozw0f))/apps/webstore/pages/Product.aspx?org=14609&pid=273")
+}
