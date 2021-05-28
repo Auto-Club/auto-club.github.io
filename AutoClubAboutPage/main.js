2d3f87f0341d9a674869f20364c03b10d6ba53c2
Auto-Club <84959631+Auto-Club@users.noreply.github.com>
2021-05-28 00:44:24 -0700

    Delete AutoClubAboutPage directory

--- AutoClubAboutPage/main.js
+++
@@ -1,4 +0,0 @@
-
-function donatePage(){
-    window.location.replace("https://redondounionasb.myschoolcentral.com/asbworks/(S(x5hwieldq30pv5dqv0dfvxl4))/apps/webstore/pages/Webstore.aspx?org=14609&cat=66")
-}
